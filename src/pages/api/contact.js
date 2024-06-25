b5c2ad37205144ffbc696af695ec0cd4cd93b743
Didrik Liu <didliu@kth.se>
2024-06-25 11:10:12 +0200

    ESLint change for vercel

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -4,7 +4,7 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { name, email, message } = req.body;
+    const {name, email, message} = req.body;
 
     const msg = {
       to: process.env.CONTACT_EMAIL, // Your email address
@@ -16,12 +16,12 @@ export default async function handler(req, res) {
 
     try {
       await sendgrid.send(msg);
-      res.status(200).json({ message: 'Email sent successfully' });
+      res.status(200).json({message: 'Email sent successfully'});
     } catch (error) {
       console.error('Error sending email:', error);
-      res.status(500).json({ message: 'Failed to send email' });
+      res.status(500).json({message: 'Failed to send email'});
     }
   } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({message: 'Method not allowed'});
   }
 }
